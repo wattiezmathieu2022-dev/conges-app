1be7e5c84e1510121ad6fd5c52fddefac097cfe2
wattiezmathieu2022-dev <wattiezmathieu2022@gmail.com>
2026-02-02 18:42:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+ const CACHE = "conges-app-cache-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
